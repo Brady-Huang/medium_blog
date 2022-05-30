985ca9023167520ba428c9b888b1e3145dbea8b8
Brady <brady120915@gmail.com>
2022-05-30 17:58:15 +0800

    create model comments

--- app/models/story.rb
+++ app/models/story.rb
@@ -12,7 +12,7 @@ class Story < ApplicationRecord
   ## relationships
   belongs_to :user
   has_one_attached :cover_image
-
+  has_many :comments
   # scopes
   ########## comment it because paranoia
   # default_scope {where(deleted_at: nil)}

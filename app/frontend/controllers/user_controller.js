0c27b670077c02464157b2bc58ed9c1a9a89442a
Brady <brady120915@gmail.com>
2022-06-01 23:23:04 +0800

    finish bookmark function

--- app/frontend/controllers/user_controller.js
+++ app/frontend/controllers/user_controller.js
@@ -2,13 +2,13 @@ import { Controller } from "stimulus"
 import axios from 'axios'
 
 export default class extends Controller {
-  static targets = [ "followButton" ]
+  static targets = [ "followButton", "bookmark" ]
 
   follow(event) {
     event.preventDefault();
     
     let user_id = this.followButtonTarget.dataset.user;
-    let button = this.followButtonTarget;
+    let button = this.followButtonTarget
     axios.post(`/api/v1/users/${user_id}/follow`)
          .then(function(response) {
            let status = response.data.status;
@@ -26,6 +26,30 @@ export default class extends Controller {
          })
          .catch(function(error) {
            console.log(error)
+         })
+  }
+
+  bookmark(event) {
+    
+    event.preventDefault();
+    let slug = event.currentTarget.dataset.slug;
+    let icon = this.bookmarkTarget
+    axios.post(`/api/v1/stories/${slug}/bookmark`)
+         .then(function(response) {
+           let status = response.data.status
+            switch (status) {
+              case 'Bookmarked':
+                icon.classList.add('fas')
+                icon.classList.remove('far')
+                break;
+              case 'Unbookmarked':
+                icon.classList.add('far')
+                icon.classList.remove('fas')
+                break;
+            }
+         })
+         .catch(function(error) {
+           console.log(error);
          })
   }
 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+        
   ## relationships
   has_many :stories
   has_many :follows
+  has_many :bookmarks
   has_one_attached :avatar
   ## validation
   validates :username, presence: true, uniqueness: true
@@ -23,6 +24,20 @@ class User < ApplicationRecord
     else
       follows.create(following: user)
       return 'Followed'
+    end
+  end
+
+  def bookmark?(story)
+    bookmarks.exists?(story: story) # true/ false 
+  end
+
+  def bookmark!(story)
+    if bookmark?(story)
+      bookmarks.find_by(story: story).destroy
+      return "Unbookmarked"
+    else
+      bookmarks.create(story: story)
+      return 'Bookmarked'
     end
   end
 end
